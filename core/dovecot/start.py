f480baa8f93c74d739385afb27e553fd4da016ba
Jumper78 <52802286+Jumper78@users.noreply.github.com>
2025-12-15 21:02:37 +0100

    Remove start_podop function and related calls
    Removed the start_podop function and its invocation.

--- core/dovecot/start.py
+++ core/dovecot/start.py
@@ -11,15 +11,6 @@ system.set_env(log_filters=[
     rb'Error\: SSL context initialization failed, disabling SSL\: Can\'t load SSL certificate \(ssl_cert setting\)\: The certificate is empty$'
 ])
 
-def start_podop():
-    system.drop_privs_to('mail')
-    url = "http://" + os.environ["ADMIN_ADDRESS"] + ":8080/internal/dovecot/§"
-    run_server(0, "dovecot", "/tmp/podop.socket", [
-		("quota", "url", url ),
-		("auth", "url", url),
-		("sieve", "url", url),
-    ])
-
 # Actual startup script
 for dovecot_file in glob.glob("/conf/*.conf"):
     conf.jinja(dovecot_file, os.environ, os.path.join("/etc/dovecot", os.path.basename(dovecot_file)))
@@ -34,6 +25,5 @@ for script_file in glob.glob("/conf/*.script"):
 os.system("chown mail:mail /mail")
 os.system("chown -R mail:mail /var/lib/dovecot /conf")
 
-multiprocessing.Process(target=start_podop).start()
 cmd = ['/usr/sbin/dovecot', '-c', '/etc/dovecot/dovecot.conf', '-F']
 system.run_process_and_forward_output(cmd)
